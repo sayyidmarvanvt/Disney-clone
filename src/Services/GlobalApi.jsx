cf25b6b95c2ec1a90e254e524e19769b4d343d9a
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-06-11 20:41:49 +0530

    add ENV file

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const movieBaseUrl = "https://api.themoviedb.org/3/";
-const api_key = "82bb8247faa0d9a66a20ef7d4856ea61";
+const api_key = import.meta.env.VITE_API_KEY;
 
 const getTrendingVideos = () => axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key);
 const getMovieByGenreId = (id) => axios.get(movieBaseUrl + "discover/movie?api_key=" + api_key + "&with_genres=" + id);
